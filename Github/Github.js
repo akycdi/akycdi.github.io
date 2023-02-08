6f824b91d2ee582df479024cd03ba47216013002
Arun Kumar <arun36824@gmail.com>
2023-02-08 11:43:10 +0530

    WIP

--- Github/Github.js
+++ Github/Github.js
@@ -31,44 +31,7 @@ async function getRepos(username) {
 async function displayRepos(username) {
   const repos = await getRepos(username);
   var cardContainer = document.getElementById('cardcontainer');
-  repos.forEach((item) => {
-    var columncard = document.createElement('div');
-    var card = document.createElement('div')
-    var card_inner = document.createElement("div");
-    var button = document.createElement('button');
-    var h2Name = document.createElement('h6');
-    var paragraph = document.createElement('p');
-    var canvas = document.createElement('canvas')
-    card_inner.className = "card-body"
-    card.className = "card cardStyle";
-    columncard.className = "col-md-4 col-sm-6 col-lg-3";
-    paragraph.appendChild(document.createTextNode(item.description));
-    h2Name.appendChild(document.createTextNode(item.name));
-
-    button.type = 'button';
-    button.innerHTML = item.name;
-    button.className = 'btn btn-primary btn-sm';
-
-    button.onclick = function () {
-      window.open(item.html_url)
-    };
-    card_inner.appendChild(h2Name);
-    card_inner.appendChild(paragraph);
-    card_inner.appendChild(button);
-    card_inner.appendChild(canvas);
-    card.appendChild(card_inner);
-    columncard.appendChild(card);
-    cardContainer.appendChild(columncard);
-
-  });
-
-  // var i = 0;
-  // repos.forEach(element => {
-  //   console.log(i++);
-  //   console.log(element.name);
-  //   console.log(element.url);
-
-  // });
+  
 }
 
 
