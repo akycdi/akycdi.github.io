0e589e882f0b78df82cc11be18cb500df2e8960d
Arun Kumar <arun36824@gmail.com>
2023-02-13 16:27:25 +0530

    WIP

--- Github/Github.js
+++ Github/Github.js
@@ -1,61 +1,23 @@
+const username = akycdi;
+async function getRepos(username) {
+  const response = await fetch(`https://api.github.com/users/${username}/repos`);
+  const repos = await response.json();
+  return repos;
+}
 
-import _ from 'lodash';
-const fetchRepoData = (repo) => {
-  var data = { key: 'a720778ddde2c87f0e92572e2c42bdc0', q: repo.html_url };
-
-  fetch('https://api.linkpreview.net', {
-    method: 'POST',
-    mode: 'cors',
-    body: JSON.stringify(data),
-  })
-    .then(res => res.json())
-    .then(response => {
-      const container = document.createElement('div');
-      container.classList.add('container');
-
-      const box = document.createElement('div');
-      box.classList.add('box');
-      box.style.width = '300px';
-      container.appendChild(box);
-
-      const image = document.createElement('img');
-      image.id = 'myimage';
-      image.src = response.image;
-      box.appendChild(image);
-
-      const content = document.createElement('div');
-      content.classList.add('is-clipped');
-      box.appendChild(content);
-
-      const title = document.createElement('div');
-      title.id = 'mytitle';
-      title.classList.add('has-text-weight-bold');
-      title.innerHTML = response.title;
-      content.appendChild(title);
-
-      const description = document.createElement('div');
-      description.id = 'mydescription';
-      description.classList.add('mt-2');
-      description.innerHTML = response.description;
-      content.appendChild(description);
-
-      const link = document.createElement('a');
-      link.id = 'myurl';
-      link.classList.add('mt-2', 'is-size-7');
-      link.innerHTML = response.url;
-      link.href = response.url;
-      content.appendChild(link);
-
-      document.body.appendChild(container);
-    });
-};
-
-fetch(`https://api.github.com/users/akycdi/repos`)
-  .then(response => response.json())
-  .then(repos => {
-    _.throttle(() => {
-      repos.forEach(repo => {
-        fetchRepoData(repo);
-      });
-    }, 1000);
+async function displayRepos(username) {
+  const repos = await getRepos(username);
+  const repoContainer = document.querySelector("#repo-container");
+  const usernameEl = document.querySelector("#username");
+  usernameEl.textContent = username;
+  const repoEl = document.querySelector("#repos");
+  repoEl.innerHTML = "";
+  repos.forEach(repo => {
+    const repoEl = document.createElement("div");
+    repoEl.innerHTML = `
+      <h3><a href="${repo.html_url}">${repo.name}</a></h3>
+      <p>${repo.description}</p>
+    `;
+    repoContainer.appendChild(repoEl);
   });
+}
